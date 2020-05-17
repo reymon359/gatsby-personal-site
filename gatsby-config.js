88487956a38f2151ffbd039430696ca695dd1a0a
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-17 22:58:02 +0200

    chore: update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,36 +2,76 @@ module.exports = {
   siteMetadata: {
     title: `ramonmorcillo.com`,
     description: `My personal site made with Gatsby.js`,
-    siteUrl: 'https://ramonmorcillo.com',
+    siteUrl: `https://ramonmorcillo.com`,
     author: {
-      name: 'Ramon Morcillo',
+      name: `Ramon Morcillo`,
       intro: `Hi, I am Ramon Morcillo, a software engineer based in Madrid, Spain`,
-      url: 'https://ramonmorcillo.com',
-      email: 'hey@ramonmorcillo.com'
+      url: `https://ramonmorcillo.com`,
+      email: `hey@ramonmorcillo.com`
     },
     social: {
-      twitter: 'https://twitter.com/reymon359',
-      github: 'https://github.com/reymon359',
-      codepen: 'https://codepen.io/reymon359',
-      linkedin: 'https://www.linkedin.com/in/ramon-morcillo/'
+      twitter: `https://twitter.com/reymon359`,
+      github: `https://github.com/reymon359`,
+      codepen: `https://codepen.io/reymon359`,
+      linkedin: `https://www.linkedin.com/in/ramon-morcillo/`
     }
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/content/posts`,
+        name: `posts`
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+        name: `images`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1280
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
+          },
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://ramonmorcillo.com`
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        shortName: `starter`,
+        name: `Ramon Morcillo`,
+        shortName: `ramonmorcillo.com`,
         startUrl: `/`,
         backgroundColor: `#663399`,
         themeColor: `#663399`,
@@ -39,16 +79,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `` // TODO: add tracking id
+      }
+    },
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-typescript`,
-    `gatsby-transformer-sharp`,
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
-    'gatsby-transformer-json',
-    'gatsby-plugin-react-svg',
-    'gatsby-plugin-styled-components'
+    `gatsby-plugin-sitemap`,
+    `gatsby-transformer-json`,
+    `gatsby-plugin-react-svg`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
