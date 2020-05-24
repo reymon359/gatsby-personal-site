2e3fbd65b5ef0bf5f5d4a8f69aa019b3865fe7af
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-24 13:02:54 +0200

    refactor: refactor 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
+import {graphql} from 'gatsby';
 
-import Layout from '../components/Layout';
-import Head from '../components/Head';
+import Layout from '../components/layout';
+import Head from '../components/head';
 
-const NotFoundPage = () => (
-  <Layout>
-    <Head title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+interface Props {
+  readonly data: PageQueryData;
+}
 
-export default NotFoundPage;
+export default class NotFoundPage extends React.Component<Props> {
+  render() {
+    const {data} = this.props;
+    const siteTitle = data.site.siteMetadata.title;
+
+    return (
+      <Layout title={siteTitle}>
+        <Head title="404: Not Found" />
+        <h1>Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Layout>
+    );
+  }
+}
+
+interface PageQueryData {
+  site: {
+    siteMetadata: {
+      title: string;
+    };
+  };
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
