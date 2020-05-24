f9dea338f2ce84bd49c08aa665affbb5e8170627
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-24 14:04:45 +0200

    fix: fix import in post and images url

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/static/images`,
         name: `images`
       }
     },
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link, graphql} from 'gatsby';
-import {styled} from '../styles/theme';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import Head from '../components/head';
