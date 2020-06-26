a16c8b41609e1172b82ce120ddde27a47538d3c3
reymon359 <ramonmorcillocascales@gmail.com>
2020-06-27 00:29:08 +0200

    refactor: create utils and add methods there

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const getRandom = (max = 10, min = 1) =>
-  Math.floor(Math.random() * max) + min;
-
-const getRandomColor = () =>'#' + ('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
-
-const getBackground = () => {
-  let background = [];
-  for (let i = 0; i < getRandom(); i++) {
-    background.push(`
-      radial-gradient(
-        circle at ${getRandom(100)}% ${getRandom(100)}%,
-        ${getRandomColor() + '0d'},
-        transparent ${getRandom(100, 60)}%
-      )
-  `);
-  }
-  return background.join(',');
-};
+import {getRandomBackground} from '../utils';
 
 const StarsContainer = styled.div`
   width: 100%;
   height: 100vh;
   background-color: ${props => props.theme.colors.black};
-  background-image: ${getBackground()};
+  background-image: ${getRandomBackground()};
 `;
 
 type Coordinates = {
