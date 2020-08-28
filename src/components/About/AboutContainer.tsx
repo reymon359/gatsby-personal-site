68102c3e3e23ee845ee56d65633353717d5f5f8b
reymon359 <ramonmorcillocascales@gmail.com>
2020-08-28 16:52:16 +0200

    feat: create about component

--- src/components/About/AboutContainer.tsx
+++ src/components/About/AboutContainer.tsx
@@ -4,16 +4,39 @@ import Intro from '../Intro';
 import {AboutMe} from './AboutMe';
 import {Image} from './Image';
 
-const Information = styled.div``;
+export const Wrapper = styled.section`
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  position: relative;
+  ${props => props.theme.media.sm`
+    grid-template-columns: 100%;
+    grid-template-rows: repeat(3, auto);
+    grid-row-gap: 2rem;
+  `}
+`;
 
-export const NavigationContainer: React.FC = () => {
+export const Information = styled.header`
+  grid-row: 2 / 4;
+  grid-column: 1 / 6;
+  z-index: 3;
+  ${props => props.theme.media.md`
+    grid-column: 1 / 7;
+  `}
+  ${props => props.theme.media.sm`
+    grid-row: 2 / 3;
+    grid-column: 1 / 1;
+  `}
+`;
+
+export const AboutContainer: React.FC = () => {
   return (
-    <>
+    <Wrapper>
       <Information>
         <Intro fixed={false} />
         <AboutMe />
       </Information>
       <Image />
-    </>
+    </Wrapper>
   );
 };
