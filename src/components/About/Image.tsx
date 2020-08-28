91b5793bb4af12cabf6280f372ad7025532a7732
reymon359 <ramonmorcillocascales@gmail.com>
2020-08-28 16:51:48 +0200

    feat: create image component

--- src/components/About/Image.tsx
+++ src/components/About/Image.tsx
@@ -1,5 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const StyledPicture = styled.picture`
+  grid-area: 1 / 4 / last-line / end;
+  background: ${props => props.theme.primaryDark};
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: '';
+    padding-top: 150%;
+    display: block;
+    ${props => props.theme.media.sm`
+      padding-top: 100%;
+    `}
+  }
+
+  ${props => props.theme.media.sm`
+    grid-area: 1 / 1 / 1 / 1;
+    &:before {
+      padding-top: 100%;
+    }
+  `}
+`;
+
+const StyledImage = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  top: -100%;
+  right: -100%;
+  bottom: -100%;
+  left: -100%;
+  mix-blend-mode: lighten;
+  opacity: 0.5;
+  display: block;
+  object-fit: cover;
+  ${props => props.theme.media.sm`
+      opacity: 0.7;
+    `}
+`;
 export const Image: React.FC = () => {
-  return <>Image</>;
+  return (
+    <StyledPicture>
+      <StyledImage src="../../../images/ramon-morcillo-meta-image.jpg" />
+    </StyledPicture>
+  );
 };
