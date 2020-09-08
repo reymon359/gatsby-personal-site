3e9434dd17ba813ba2fec19cce8390f73cc0d857
reymon359 <ramonmorcillocascales@gmail.com>
2020-09-08 19:44:46 +0200

    feat: refactor remove nodes to an array

--- src/hooks/useFruitFavicon/useFruitFavicon.tsx
+++ src/hooks/useFruitFavicon/useFruitFavicon.tsx
@@ -22,18 +22,13 @@ const changeFavicon = (origin = 'https://ramonmorcillo.com') => {
   const head = document.head ?? document.getElementsByTagName('head')[0];
   const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
   const oldLink = document.querySelectorAll(`[href*='/favicon']`)[0];
-  const link = document.createElement('link');
 
+  const link = document.createElement('link');
   link.className = 'dynamic-favicon';
   link.rel = 'shortcut icon';
   link.href = `${origin}/images/favicon/${randomFruit}.png`;
+
   oldLink && head.removeChild(oldLink);
-
-  const nodes = Array.from(document.getElementsByClassName('dynamic-favicon'))
-  nodes?.forEach(node => {
-    head.removeChild(node);
-  })
-
   head.appendChild(link);
 };
 
