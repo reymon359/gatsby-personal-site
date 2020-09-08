5f8b939fa9e2827e625e6c910244f3807894aaed
reymon359 <ramonmorcillocascales@gmail.com>
2020-09-08 19:52:15 +0200

    refactor: add get random fruit to miscellaneous

--- src/utils/misc.tsx
+++ src/utils/misc.tsx
@@ -1,2 +1,23 @@
 export const getRandomNumber = (max = 10, min = 1) =>
   Math.floor(Math.random() * max) + min;
+export const getRandomFruit = () => {
+  const fruits = [
+    'kiwi',
+    'coconut',
+    'grapes',
+    'melon',
+    'watermelon',
+    'orange',
+    'lemon',
+    'banana',
+    'pineapple',
+    'red_apple',
+    'green_apple',
+    'pear',
+    'peach',
+    'cherries',
+    'strawberry'
+  ];
+
+  return fruits[Math.floor(Math.random() * fruits.length)];
+};
