8db4379f0122c0f21dcea25c75b7561c35a84f70
reymon359 <ramonmorcillocascales@gmail.com>
2020-09-08 19:43:46 +0200

    feat: refactor and add nodes to an array

--- src/hooks/useFruitFavicon/useFruitFavicon.tsx
+++ src/hooks/useFruitFavicon/useFruitFavicon.tsx
@@ -18,32 +18,24 @@ const fruits = [
   'strawberry'
 ];
 
-document.head || (document.head = document.getElementsByTagName('head')[0]);
-const head = document.head ?? document.getElementsByTagName('head')[0];
-
 const changeFavicon = (origin = 'https://ramonmorcillo.com') => {
-  console.log('vhanging favicon');
+  const head = document.head ?? document.getElementsByTagName('head')[0];
   const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
-  console.log(randomFruit);
   const oldLink = document.querySelectorAll(`[href*='/favicon']`)[0];
-  console.log(oldLink);
   const link = document.createElement('link');
-  // const  oldLink = document.getElementById('dynamic-favicon');
+
   link.className = 'dynamic-favicon';
   link.rel = 'shortcut icon';
   link.href = `${origin}/images/favicon/${randomFruit}.png`;
-  console.log(link);
-  if (oldLink) {
-    document.head.removeChild(oldLink);
-  }
-  const nodes = document.getElementsByClassName('dynamic-favicon');
-  for (let i = 0; i < nodes.length; i++) {
-    nodes[i].parentNode.removeChild(nodes[i]);
-  }
+  oldLink && head.removeChild(oldLink);
 
-  document.head.appendChild(link);
+  const nodes = Array.from(document.getElementsByClassName('dynamic-favicon'))
+  nodes?.forEach(node => {
+    head.removeChild(node);
+  })
+
+  head.appendChild(link);
 };
-const els = document.querySelectorAll("a[href='http://domain.com']");
 
 export const useFruitFavicon = () => {
   const location = window?.location;
@@ -53,20 +45,4 @@ export const useFruitFavicon = () => {
     console.log('location changed', location);
     changeFavicon(location.origin);
   }, [location.pathname]);
-
-  // const [isMobile, setMobile] = React.useState(false);
-
-  // React.useEffect(() => {
-  //   // const userAgent =
-  //   //   typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
-  //   // const mobile = Boolean(
-  //   //   userAgent.match(
-  //   //     /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
-  //   //   )
-  //   // );
-  //   // setMobile(mobile);
-  // }, []);
-
-  // return {isMobile};
-  // return (  <link id="favicon" rel="icon" href="images/favicon/pear.png" type="image/x-icon"  />)
 };
