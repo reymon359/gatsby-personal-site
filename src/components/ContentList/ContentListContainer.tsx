ebbacf452528ae6e19dfc2fdb7805e9c029bd2b7
reymon359 <ramonmorcillocascales@gmail.com>
2020-09-09 23:10:05 +0200

    refactor: remove bracets from container

--- src/components/ContentList/ContentListContainer.tsx
+++ src/components/ContentList/ContentListContainer.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {ContentList} from './ContentList';
 import {Work} from '../../types';
 
-
-
 interface ContentListProps {
   readonly type?: boolean;
   readonly content: Work[];
@@ -12,12 +10,10 @@ interface ContentListProps {
 export const ContentListContainer: React.FC<ContentListProps> = ({
   type,
   content
-}) => {
-  return (
-    <>
-      {content.map(({node}) => (
-        <ContentList key={node.fields.slug} type={type} node={node} />
-      ))}
-    </>
-  );
-};
+}) => (
+  <>
+    {content.map(({node}) => (
+      <ContentList key={node.fields.slug} type={type} node={node} />
+    ))}
+  </>
+);
