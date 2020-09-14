01f345afe2d316deaa7c43c3e98d34957e83372f
reymon359 <ramonmorcillocascales@gmail.com>
2020-09-14 17:38:29 +0200

    feat: add home component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Intro from '../Intro';
+
+const Home: React.FC = () => <Intro fixed={true} />;
+
+export default Home;
---
+++ src/components/Home/HomeContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './Home';
+
+const HomeContainer: React.FC = () => {
+  return <Home />;
+};
+
+export default HomeContainer;
