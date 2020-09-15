1fa11dcf4abf0e0db27df68f836ddc128fdd38cc
reymon359 <ramonmorcillocascales@gmail.com>
2020-09-15 19:07:41 +0200

    feat: create reactions and avatar components

---
+++ src/components/Comments/Avatar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface AvatarProps {
+  commentsUrl: string;
+}
+
+const Avatar: React.FC<AvatarProps> = () => {
+  return <h1>reactions</h1>;
+};
+
+export default Avatar;
---
+++ src/components/Comments/Reactions.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ReactionsProps {
+  commentsUrl: string;
+}
+
+const Reactions: React.FC<ReactionsProps> = () => {
+  return <h1>reactions</h1>;
+};
+
+export default Reactions;
