2a0b87cd7f0793042aac07243b0a685e2b14e042
reymon359 <ramonmorcillocascales@gmail.com>
2025-12-19 17:28:51 +0700

    refactor: update About page content to enhance personal expression and simplify messaging

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 
 const About: React.FC = () => {
     return (
@@ -11,28 +10,11 @@ const About: React.FC = () => {
             </div>
             <section className="prose prose-invert max-w-none text-gray-300">
                 <p>
-                    Problem solver and experienced maker with a passion for excellence and pragmatism.
+                    I do many things while trying to enjoy the passage of time.
                 </p>
+                
                 <p>
-                    I create software experiences that bring joy to people&apos;s lives.
-                </p>
-                <p>
-                    I also guide individuals and teams through personalized <a href="https://coach.ramonmorcillo.com" target="_blank" rel="noopener noreferrer" className="cursor-pointer no-underline transition-colors ease-in duration-200 font-bold text-white border-b border-dashed border-white hover:border-solid">coaching and mentoring services</a>.
-                </p>
-                <p>
-                    This website serves as a platform for sharing my knowledge and achievements. You can find my resume <Link href="/resume" className="cursor-pointer no-underline transition-colors ease-in duration-200 font-bold text-white border-b border-dashed border-white hover:border-solid">here</Link>.
-                </p>
-                <p>
-                    Occasionally, I&nbsp;
-                    <a href="https://twitter.com/reymon359" target="_blank" rel="noopener noreferrer" className="cursor-pointer no-underline transition-colors ease-in duration-200 font-bold text-white border-b border-dashed border-white hover:border-solid">share insights</a>
-                    &nbsp;and post&nbsp;
-                    <a href="https://www.instagram.com/ramon.mor/" target="_blank" rel="noopener noreferrer" className="cursor-pointer no-underline transition-colors ease-in duration-200 font-bold text-white border-b border-dashed border-white hover:border-solid">moments</a>
-                    &nbsp;or&nbsp;
-                    <a href="https://www.instagram.com/ramon.mor.photos/" target="_blank" rel="noopener noreferrer" className="cursor-pointer no-underline transition-colors ease-in duration-200 font-bold text-white border-b border-dashed border-white hover:border-solid">images</a>
-                    &nbsp;on Instagram.
-                </p>
-                <p>
-                    Feel free to send me an email to&nbsp;
+                    Feel free to reach out at&nbsp;
                     <a href="mailto:hey@ramonmorcillo.com?subject=Hi%20there!" rel="noopener" className="cursor-pointer no-underline transition-colors ease-in duration-200 font-bold text-white border-b border-dashed border-white hover:border-solid">hey@ramonmorcillo.com</a>.
                 </p>
             </section>
